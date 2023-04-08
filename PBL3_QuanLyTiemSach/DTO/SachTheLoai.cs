64d256333c4c71ffd3fb2a961e0242569a8272c2
LNHBKDN <102210055@sv1.dut.udn.vn>
2023-04-08 21:44:45 +0700

    CF Hanh release

---
+++ PBL3_QuanLyTiemSach/DTO/SachTheLoai.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PBL3_QuanLyTiemSach.DTO
+{
+    public class SachTheLoai
+    {
+        [Key]
+        [Required]
+        [StringLength(20)]
+        public string ID { get; set; }
+        [StringLength(20)]
+        public string TenTheLoai { get; set; }
+
+        public virtual ICollection<Sach> Saches { get; set; }
+    }
+}
