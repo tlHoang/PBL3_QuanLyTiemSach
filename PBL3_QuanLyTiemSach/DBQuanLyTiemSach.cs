72367091d51d7c84889547b623c21453802661bf
Huy Hoang <tlhh232003@gmail.com>
2023-04-09 22:04:23 +0700

    Update class DB

--- PBL3_QuanLyTiemSach/DBQuanLyTiemSach.cs
+++ PBL3_QuanLyTiemSach/DBQuanLyTiemSach.cs
@@ -1,3 +1,4 @@
+using PBL3_QuanLyTiemSach.DTO;
 using System;
 using System.Data.Entity;
 using System.Linq;
@@ -21,6 +22,18 @@ namespace PBL3_QuanLyTiemSach
         // on configuring and using a Code First model, see http://go.microsoft.com/fwlink/?LinkId=390109.
 
         // public virtual DbSet<MyEntity> MyEntities { get; set; }
+        public virtual DbSet<Ca> Cas { get; set; }
+        public virtual DbSet<CaNV> CaNVs { get; set; }
+        public virtual DbSet<DonViCungCap> DonViCungCaps { get; set; }
+        public virtual DbSet<HoaDonBan> HoaDonBans { get; set; }
+        public virtual DbSet<HoaDonBanSach> HoaDonBanSachs { get; set; }
+        public virtual DbSet<HoaDonNhap> HoaDonNhaps { get; set; }
+        public virtual DbSet<HoaDonNhapSach> HoaDonNhapSachs { get; set; }
+        public virtual DbSet<KhachHang> KhachHangs { get; set; }
+        public virtual DbSet<Kho> Khos { get; set; }
+        public virtual DbSet<NhanVien> NhanViens { get; set; }
+        public virtual DbSet<Sach> Sachs { get; set; }
+        public virtual DbSet<SachTheLoai> SachTheLoais { get; set; }
     }
 
     //public class MyEntity
