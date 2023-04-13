0968eb3f2d0bc5bbbfb8e7166cb93203ae1f0bc8
Huy Hoang <tlhh232003@gmail.com>
2023-04-13 11:06:54 +0700

    update account info

--- PBL3_QuanLyTiemSach/DTO/CodeFirst/Account.cs
+++ PBL3_QuanLyTiemSach/DTO/CodeFirst/Account.cs
@@ -19,6 +19,8 @@ namespace PBL3_QuanLyTiemSach.DTO.CodeFirst
         [Required]
         [StringLength(30)]
         public string Password { get; set; }
+        [Required]
+        public string Salt { get; set; }
 
         public virtual NhanVien NhanVien { get; set; }
     }
