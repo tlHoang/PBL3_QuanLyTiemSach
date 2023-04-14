b32211bd22587502f0456f11c92e63ccf4b69f63
LNHBKDN <102210055@sv1.dut.udn.vn>
2023-04-14 11:45:24 +0700

    Allitle bit code for book UI

--- PBL3_QuanLyTiemSach/Program.cs
+++ PBL3_QuanLyTiemSach/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PBL3_QuanLyTiemSach.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -16,7 +17,7 @@ namespace PBL3_QuanLyTiemSach
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new BookInfo());
         }
     }
 }
--- PBL3_QuanLyTiemSach/View/BookInfo.cs
+++ PBL3_QuanLyTiemSach/View/BookInfo.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PBL3_QuanLyTiemSach.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,50 @@ namespace PBL3_QuanLyTiemSach.View
         public BookInfo()
         {
             InitializeComponent();
+            setBookInfoDgv();
+        }
+        private void setBookInfoDgv()
+        {
+            using(DBQuanLyTiemSach db = new DBQuanLyTiemSach())
+            {
+                List<Sach> sach = db.Sachs.ToList();
+                List<Kho> kho = db.Khos.ToList();
+                var dataView = sach.Select(s => new
+                {
+                    ID = s.MaSach,
+                    TenSach = s.TenSach,
+                    SL = kho.Where(k => k.MaKho == s.MaKho).Select(k => k.SoLuongSachConLai).Single()
+                }).ToList();
+                dgvBookInfo.DataSource = dataView;
+            }
+            dgvBookInfo.Columns["ID"].HeaderText = "Mã Sách";
+            dgvBookInfo.Columns["TenSach"].HeaderText = "Tên Sách";
+            dgvBookInfo.Columns["SL"].HeaderText = "Số Lượng";
+
+        }
+
+        private void btnBookInfoTimKiem_Click(object sender, EventArgs e)
+        {
+            string txtTenSach = txtBookInfoTenSach.Text;
+            string txtTacGia = txtBookInfoTacGia.Text;
+            using(DBQuanLyTiemSach db = new DBQuanLyTiemSach())
+            {
+                List<Sach> sach = db.Sachs.ToList();
+                List<Kho> kho = db.Khos.ToList();
+                List<SachTheLoai> theLoai = db.SachTheLoais.ToList();
+                var dataView = sach.Select(s => new
+                {
+                    ID = s.MaSach,
+                    TenSach = s.TenSach,
+                    TheLoai = theLoai.Where( tl => tl.MaTheLoai == s.MaTheLoai).Select(tl => tl.TenTheLoai).Single(),
+                    SL = kho.Where(k => k.MaKho == s.MaKho).Select(k => k.SoLuongSachConLai).Single()
+                }).ToList();
+                dgvBookInfo.DataSource = dataView;
+            }
+            dgvBookInfo.Columns["ID"].HeaderText = "Mã Sách";
+            dgvBookInfo.Columns["TenSach"].HeaderText = "Tên Sách";
+            dgvBookInfo.Columns["TheLoai"].HeaderText = "Thể Loại";
+            dgvBookInfo.Columns["SL"].HeaderText = "Số Lượng";
         }
     }
 }
