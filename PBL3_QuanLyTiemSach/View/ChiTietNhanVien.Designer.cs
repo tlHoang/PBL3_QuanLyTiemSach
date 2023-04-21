09b924ab488034da4afc0e08d14a0fba8b9b171e
tlHoang <tlhh232003@gmail.com>
2023-04-21 14:07:59 +0700

    UI chi tiet nv

---
+++ PBL3_QuanLyTiemSach/View/ChiTietNhanVien.Designer.cs
@@ -0,0 +1,324 @@
+﻿namespace PBL3_QuanLyTiemSach.View
+{
+    partial class ChiTietNhanVien
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.metroLabel1 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel2 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox_ma = new MetroFramework.Controls.MetroTextBox();
+            this.metroTextBox_ten = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel3 = new MetroFramework.Controls.MetroLabel();
+            this.metroLabel4 = new MetroFramework.Controls.MetroLabel();
+            this.metroRadioButton_nam = new MetroFramework.Controls.MetroRadioButton();
+            this.metroRadioButton_nu = new MetroFramework.Controls.MetroRadioButton();
+            this.metroLabel5 = new MetroFramework.Controls.MetroLabel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.metroTextBox_diachi = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel6 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox_luong = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel7 = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox_sdt = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel8 = new MetroFramework.Controls.MetroLabel();
+            this.metroButton_xacnhan = new MetroFramework.Controls.MetroButton();
+            this.tabControl_nhanvien = new System.Windows.Forms.TabControl();
+            this.tabPage_thongtin = new System.Windows.Forms.TabPage();
+            this.tabPage_taikhoan = new System.Windows.Forms.TabPage();
+            this.metroTextBox2 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel_matkhau = new MetroFramework.Controls.MetroLabel();
+            this.metroTextBox1 = new MetroFramework.Controls.MetroTextBox();
+            this.metroLabel_taikhoan = new MetroFramework.Controls.MetroLabel();
+            this.tabControl_nhanvien.SuspendLayout();
+            this.tabPage_thongtin.SuspendLayout();
+            this.tabPage_taikhoan.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // metroLabel1
+            // 
+            this.metroLabel1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.metroLabel1.FontWeight = MetroFramework.MetroLabelWeight.Bold;
+            this.metroLabel1.Location = new System.Drawing.Point(138, 9);
+            this.metroLabel1.Name = "metroLabel1";
+            this.metroLabel1.Size = new System.Drawing.Size(166, 33);
+            this.metroLabel1.TabIndex = 0;
+            this.metroLabel1.Text = "Chi tiết nhân viên";
+            this.metroLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // metroLabel2
+            // 
+            this.metroLabel2.Location = new System.Drawing.Point(5, 6);
+            this.metroLabel2.Name = "metroLabel2";
+            this.metroLabel2.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel2.TabIndex = 1;
+            this.metroLabel2.Text = "Mã nhân viên";
+            // 
+            // metroTextBox_ma
+            // 
+            this.metroTextBox_ma.Location = new System.Drawing.Point(111, 6);
+            this.metroTextBox_ma.Name = "metroTextBox_ma";
+            this.metroTextBox_ma.Size = new System.Drawing.Size(141, 23);
+            this.metroTextBox_ma.TabIndex = 2;
+            // 
+            // metroTextBox_ten
+            // 
+            this.metroTextBox_ten.Location = new System.Drawing.Point(111, 35);
+            this.metroTextBox_ten.Name = "metroTextBox_ten";
+            this.metroTextBox_ten.Size = new System.Drawing.Size(141, 23);
+            this.metroTextBox_ten.TabIndex = 4;
+            // 
+            // metroLabel3
+            // 
+            this.metroLabel3.Location = new System.Drawing.Point(5, 35);
+            this.metroLabel3.Name = "metroLabel3";
+            this.metroLabel3.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel3.TabIndex = 3;
+            this.metroLabel3.Text = "Tên";
+            // 
+            // metroLabel4
+            // 
+            this.metroLabel4.Location = new System.Drawing.Point(5, 64);
+            this.metroLabel4.Name = "metroLabel4";
+            this.metroLabel4.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel4.TabIndex = 5;
+            this.metroLabel4.Text = "Giới tính";
+            // 
+            // metroRadioButton_nam
+            // 
+            this.metroRadioButton_nam.AutoSize = true;
+            this.metroRadioButton_nam.Location = new System.Drawing.Point(129, 68);
+            this.metroRadioButton_nam.Name = "metroRadioButton_nam";
+            this.metroRadioButton_nam.Size = new System.Drawing.Size(49, 15);
+            this.metroRadioButton_nam.TabIndex = 6;
+            this.metroRadioButton_nam.TabStop = true;
+            this.metroRadioButton_nam.Text = "Nam";
+            this.metroRadioButton_nam.UseVisualStyleBackColor = true;
+            // 
+            // metroRadioButton_nu
+            // 
+            this.metroRadioButton_nu.AutoSize = true;
+            this.metroRadioButton_nu.Location = new System.Drawing.Point(194, 68);
+            this.metroRadioButton_nu.Name = "metroRadioButton_nu";
+            this.metroRadioButton_nu.Size = new System.Drawing.Size(39, 15);
+            this.metroRadioButton_nu.TabIndex = 7;
+            this.metroRadioButton_nu.TabStop = true;
+            this.metroRadioButton_nu.Text = "Nữ";
+            this.metroRadioButton_nu.UseVisualStyleBackColor = true;
+            // 
+            // metroLabel5
+            // 
+            this.metroLabel5.Location = new System.Drawing.Point(5, 93);
+            this.metroLabel5.Name = "metroLabel5";
+            this.metroLabel5.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel5.TabIndex = 8;
+            this.metroLabel5.Text = "Ngày sinh";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(111, 95);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 20);
+            this.dateTimePicker1.TabIndex = 10;
+            // 
+            // metroTextBox_diachi
+            // 
+            this.metroTextBox_diachi.Location = new System.Drawing.Point(111, 121);
+            this.metroTextBox_diachi.Name = "metroTextBox_diachi";
+            this.metroTextBox_diachi.Size = new System.Drawing.Size(141, 23);
+            this.metroTextBox_diachi.TabIndex = 12;
+            // 
+            // metroLabel6
+            // 
+            this.metroLabel6.Location = new System.Drawing.Point(5, 121);
+            this.metroLabel6.Name = "metroLabel6";
+            this.metroLabel6.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel6.TabIndex = 11;
+            this.metroLabel6.Text = "Địa chỉ";
+            // 
+            // metroTextBox_luong
+            // 
+            this.metroTextBox_luong.Location = new System.Drawing.Point(111, 150);
+            this.metroTextBox_luong.Name = "metroTextBox_luong";
+            this.metroTextBox_luong.Size = new System.Drawing.Size(141, 23);
+            this.metroTextBox_luong.TabIndex = 14;
+            // 
+            // metroLabel7
+            // 
+            this.metroLabel7.Location = new System.Drawing.Point(5, 150);
+            this.metroLabel7.Name = "metroLabel7";
+            this.metroLabel7.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel7.TabIndex = 13;
+            this.metroLabel7.Text = "Lương";
+            // 
+            // metroTextBox_sdt
+            // 
+            this.metroTextBox_sdt.Location = new System.Drawing.Point(111, 179);
+            this.metroTextBox_sdt.Name = "metroTextBox_sdt";
+            this.metroTextBox_sdt.Size = new System.Drawing.Size(141, 23);
+            this.metroTextBox_sdt.TabIndex = 16;
+            // 
+            // metroLabel8
+            // 
+            this.metroLabel8.Location = new System.Drawing.Point(5, 179);
+            this.metroLabel8.Name = "metroLabel8";
+            this.metroLabel8.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel8.TabIndex = 15;
+            this.metroLabel8.Text = "SĐT";
+            // 
+            // metroButton_xacnhan
+            // 
+            this.metroButton_xacnhan.Location = new System.Drawing.Point(322, 474);
+            this.metroButton_xacnhan.Name = "metroButton_xacnhan";
+            this.metroButton_xacnhan.Size = new System.Drawing.Size(75, 23);
+            this.metroButton_xacnhan.TabIndex = 17;
+            this.metroButton_xacnhan.Text = "Xác nhận";
+            // 
+            // tabControl_nhanvien
+            // 
+            this.tabControl_nhanvien.Controls.Add(this.tabPage_thongtin);
+            this.tabControl_nhanvien.Controls.Add(this.tabPage_taikhoan);
+            this.tabControl_nhanvien.Location = new System.Drawing.Point(12, 74);
+            this.tabControl_nhanvien.Name = "tabControl_nhanvien";
+            this.tabControl_nhanvien.SelectedIndex = 0;
+            this.tabControl_nhanvien.Size = new System.Drawing.Size(337, 237);
+            this.tabControl_nhanvien.TabIndex = 19;
+            // 
+            // tabPage_thongtin
+            // 
+            this.tabPage_thongtin.Controls.Add(this.metroTextBox_ma);
+            this.tabPage_thongtin.Controls.Add(this.metroLabel5);
+            this.tabPage_thongtin.Controls.Add(this.metroTextBox_sdt);
+            this.tabPage_thongtin.Controls.Add(this.metroRadioButton_nu);
+            this.tabPage_thongtin.Controls.Add(this.metroLabel2);
+            this.tabPage_thongtin.Controls.Add(this.dateTimePicker1);
+            this.tabPage_thongtin.Controls.Add(this.metroLabel8);
+            this.tabPage_thongtin.Controls.Add(this.metroRadioButton_nam);
+            this.tabPage_thongtin.Controls.Add(this.metroLabel6);
+            this.tabPage_thongtin.Controls.Add(this.metroTextBox_luong);
+            this.tabPage_thongtin.Controls.Add(this.metroLabel4);
+            this.tabPage_thongtin.Controls.Add(this.metroLabel3);
+            this.tabPage_thongtin.Controls.Add(this.metroTextBox_diachi);
+            this.tabPage_thongtin.Controls.Add(this.metroLabel7);
+            this.tabPage_thongtin.Controls.Add(this.metroTextBox_ten);
+            this.tabPage_thongtin.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_thongtin.Name = "tabPage_thongtin";
+            this.tabPage_thongtin.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_thongtin.Size = new System.Drawing.Size(329, 211);
+            this.tabPage_thongtin.TabIndex = 0;
+            this.tabPage_thongtin.Text = "Cá nhân";
+            this.tabPage_thongtin.UseVisualStyleBackColor = true;
+            // 
+            // tabPage_taikhoan
+            // 
+            this.tabPage_taikhoan.Controls.Add(this.metroTextBox2);
+            this.tabPage_taikhoan.Controls.Add(this.metroLabel_matkhau);
+            this.tabPage_taikhoan.Controls.Add(this.metroTextBox1);
+            this.tabPage_taikhoan.Controls.Add(this.metroLabel_taikhoan);
+            this.tabPage_taikhoan.Location = new System.Drawing.Point(4, 22);
+            this.tabPage_taikhoan.Name = "tabPage_taikhoan";
+            this.tabPage_taikhoan.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage_taikhoan.Size = new System.Drawing.Size(329, 211);
+            this.tabPage_taikhoan.TabIndex = 1;
+            this.tabPage_taikhoan.Text = "Tài khoản";
+            this.tabPage_taikhoan.UseVisualStyleBackColor = true;
+            // 
+            // metroTextBox2
+            // 
+            this.metroTextBox2.Location = new System.Drawing.Point(111, 35);
+            this.metroTextBox2.Name = "metroTextBox2";
+            this.metroTextBox2.Size = new System.Drawing.Size(141, 23);
+            this.metroTextBox2.TabIndex = 6;
+            // 
+            // metroLabel_matkhau
+            // 
+            this.metroLabel_matkhau.Location = new System.Drawing.Point(7, 35);
+            this.metroLabel_matkhau.Name = "metroLabel_matkhau";
+            this.metroLabel_matkhau.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel_matkhau.TabIndex = 5;
+            this.metroLabel_matkhau.Text = "Mật khẩu";
+            // 
+            // metroTextBox1
+            // 
+            this.metroTextBox1.Location = new System.Drawing.Point(111, 6);
+            this.metroTextBox1.Name = "metroTextBox1";
+            this.metroTextBox1.Size = new System.Drawing.Size(141, 23);
+            this.metroTextBox1.TabIndex = 4;
+            // 
+            // metroLabel_taikhoan
+            // 
+            this.metroLabel_taikhoan.Location = new System.Drawing.Point(5, 6);
+            this.metroLabel_taikhoan.Name = "metroLabel_taikhoan";
+            this.metroLabel_taikhoan.Size = new System.Drawing.Size(100, 23);
+            this.metroLabel_taikhoan.TabIndex = 3;
+            this.metroLabel_taikhoan.Text = "Tài khoản";
+            // 
+            // ChiTietNhanVien
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(440, 530);
+            this.Controls.Add(this.tabControl_nhanvien);
+            this.Controls.Add(this.metroButton_xacnhan);
+            this.Controls.Add(this.metroLabel1);
+            this.Name = "ChiTietNhanVien";
+            this.Text = "ChiTietNhanVien";
+            this.tabControl_nhanvien.ResumeLayout(false);
+            this.tabPage_thongtin.ResumeLayout(false);
+            this.tabPage_thongtin.PerformLayout();
+            this.tabPage_taikhoan.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroLabel metroLabel1;
+        private MetroFramework.Controls.MetroLabel metroLabel2;
+        private MetroFramework.Controls.MetroTextBox metroTextBox_ma;
+        private MetroFramework.Controls.MetroTextBox metroTextBox_ten;
+        private MetroFramework.Controls.MetroLabel metroLabel3;
+        private MetroFramework.Controls.MetroLabel metroLabel4;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton_nam;
+        private MetroFramework.Controls.MetroRadioButton metroRadioButton_nu;
+        private MetroFramework.Controls.MetroLabel metroLabel5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private MetroFramework.Controls.MetroTextBox metroTextBox_diachi;
+        private MetroFramework.Controls.MetroLabel metroLabel6;
+        private MetroFramework.Controls.MetroTextBox metroTextBox_luong;
+        private MetroFramework.Controls.MetroLabel metroLabel7;
+        private MetroFramework.Controls.MetroTextBox metroTextBox_sdt;
+        private MetroFramework.Controls.MetroLabel metroLabel8;
+        private MetroFramework.Controls.MetroButton metroButton_xacnhan;
+        private System.Windows.Forms.TabControl tabControl_nhanvien;
+        private System.Windows.Forms.TabPage tabPage_thongtin;
+        private System.Windows.Forms.TabPage tabPage_taikhoan;
+        private MetroFramework.Controls.MetroTextBox metroTextBox2;
+        private MetroFramework.Controls.MetroLabel metroLabel_matkhau;
+        private MetroFramework.Controls.MetroTextBox metroTextBox1;
+        private MetroFramework.Controls.MetroLabel metroLabel_taikhoan;
+    }
+}
